df38e15e01edebd35cff33d401b0a8e39de9c523
Adam Christie <fractos@gmail.com>
2016-05-10 20:58:45 +0100

    adding null transport.

---
+++ Inversion.Messaging/Transport/NullTransport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Inversion.Data;
+using Inversion.Process;
+
+namespace Inversion.Messaging.Transport
+{
+    public class NullTransport : StoreBase, ITransport
+    {
+        public override void Dispose()
+        {
+            // nothing to do
+        }
+
+        public IEvent Peek()
+        {
+            return null;
+        }
+
+        public long Count()
+        {
+            return 0;
+        }
+
+        public IEvent Pop()
+        {
+            return null;
+        }
+
+        public void Push(IEvent ev)
+        {
+            // nothing to do
+        }
+    }
+}
