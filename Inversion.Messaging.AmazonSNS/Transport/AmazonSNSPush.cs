e83bd57d7b8dcf4fd327dcd7ab95784da9e97951
Adam Christie <fractos@gmail.com>
2016-06-12 11:59:15 +0100

    adding amazon sns transport.

---
+++ Inversion.Messaging.AmazonSNS/Transport/AmazonSNSPush.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Amazon.SimpleNotificationService.Model;
+
+using Inversion.Data;
+using Inversion.Process;
+
+namespace Inversion.Messaging.Transport
+{
+    public class AmazonSNSPush : AmazonSNSStore, IPush
+    {
+        public AmazonSNSPush(string serviceUrl, string region, string accessKey, string accessSecret) : base(serviceUrl, region, accessKey, accessSecret) {}
+
+        public void Push(IEvent ev)
+        {
+            this.AssertIsStarted();
+
+            PublishResponse response = this.Client.Publish(new PublishRequest
+            {
+                Message = ev.ToJson(),
+                TopicArn = this.TopicArn
+            });
+        }
+    }
+}
