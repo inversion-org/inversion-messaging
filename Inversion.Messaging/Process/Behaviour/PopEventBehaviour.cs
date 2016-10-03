eab7c0469c3eec09035042cf097df0a69137af1c
Adam Christie <fractos@gmail.com>
2016-10-03 12:01:17 +0100

    adding PopEventBehaviour.

---
+++ Inversion.Messaging/Process/Behaviour/PopEventBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Inversion.Extensibility.Extensions;
+using Inversion.Messaging.Transport;
+using Inversion.Process;
+using Inversion.Process.Behaviour;
+
+namespace Inversion.Messaging.Process.Behaviour
+{
+    public class PopEventBehaviour : PrototypedBehaviour
+    {
+        public PopEventBehaviour(string respondsTo) : base(respondsTo) {}
+        public PopEventBehaviour(string respondsTo, IPrototype prototype) : base(respondsTo, prototype) {}
+        public PopEventBehaviour(string respondsTo, IEnumerable<IConfigurationElement> config) : base(respondsTo, config) {}
+
+        public override void Action(IEvent ev, IProcessContext context)
+        {
+            string service = this.Configuration.GetNameWithAssert("config", "service");
+
+            IEvent poppedEvent = null;
+
+            using (IPop pop = context.Services.GetService<IPop>(service))
+            {
+                pop.Start();
+
+                poppedEvent = pop.Pop();
+            }
+
+            if (poppedEvent != null)
+            {
+                context.Fire(poppedEvent);
+            }
+        }
+    }
+}
